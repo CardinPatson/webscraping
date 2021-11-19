6bf74a5323894fea56f8c2877b839c2dbd2c2acc
PatsonEph <c.tiakongouadje@students.ephec.be>
2021-11-19 22:50:22 +0100

    Ajout du scraping sur base de l'attribut

--- main.py
+++ main.py
@@ -1,49 +1,83 @@
+# -*- coding: utf-8 -*-
+
 # pip install requests
 # pip install bs4
 # Possible d'utiliser selenium mais il faudra connaitre le drivers
 import requests
 from bs4 import BeautifulSoup
 
+
 # Effectuer une request http avec la méthode get
 url = "https://fr.wikipedia.org/wiki/Liste_des_pays_du_monde"
 
-res = requests.get(url)
 
-if res.ok:
-    # Permet de parser et de chercher les elts à l'intérieur
-    rep = BeautifulSoup(res.text, "html.parser")
+if __name__ == "__main__":
+    try:
+        with open('./data/test.html') as fd:
+            # print(f.read())
+            soup = BeautifulSoup(fd, 'html.parser')
+            print(soup)
+    except FileNotFoundError:
+        print("Fichier introuvable")
 
-    rep.prettify()
-    print(rep.title)
-    print(rep.title.string)  # afficher  le contenu de la balise
+    tab_body = soup.body.content
 
-    print(rep.find_all("a"))  # rechercher toutes les balises <a>
+    print(tab_body)
 
-    print(rep.find(id="États_dont_la_souveraineté_est_contestée"))
+    # Possible d'acceder au attribut d'une balise en traitant la balise comme un dictionnaire
 
-    # Preciser le parser que tu veux
-    # print(rep.find("title"))
+    print(soup.p["id"])
 
-    # extract url in <a> tag in a page
-    for link in rep.find_all('a'):
-        print(link.get("href"))
+    # Pour acceder directement au dictionnaire
 
-    text_page = rep.get_text()  # extraire tous le texte de la page
+    print(soup.p.attrs)
 
-    # ==================================
-    # NAVIGER A PARTIR DES NOM DES TAGS
-    # ==================================
+    res = requests.get(url)
+    if not res.ok:
+        # Permet de parser et de chercher les elts à l'intérieur
+        rep = BeautifulSoup(res.text, "html.parser")
 
-    # .content and .children
-    print(rep.body)
+        rep.prettify()
+        print(rep.title)  # Affiche la balise title et son contenu
 
-    # Met tout ce qui se trouve dans le head dans un tableau
-    print(rep.head.content)
+        print(rep.title.name)  # Affiche le nom de la balise
 
-    # iterer sur les balises enfants
-    for child in rep.head.children:
-        print(child)
+        print(rep.title.string)  # afficher  le contenu de la balise title
 
+        print(rep.title.parent)  # Affiche le parent direct de title
 
-# print(res.headers)
-# print(res.text)  #res.text si on veut afficher la pagesu
+        print(rep.p)  # Affiche le premier p dans toute la page
+
+        print(rep.p["class"])  # Affiche la classe de p
+
+        print(rep.find_all("a"))  # rechercher toutes les balises <a>
+
+        print(rep.find(id="États_dont_la_souveraineté_est_contestée"))
+
+        # Preciser le parser que tu veux
+
+        # print(rep.find("title"))
+
+        # extract url in <a> tag in a page
+
+        for link in rep.find_all('a'):
+            print(link.get("href"))
+
+        text_page = rep.get_text()  # extraire tous le texte de la page
+
+        # ==================================
+        # NAVIGER A PARTIR DES NOM DES TAGS
+        # ==================================
+
+        # .content and .children
+        print(rep.body)
+
+        # Met tout ce qui se trouve dans le head dans un tableau
+        print(rep.head.content)
+
+        # iterer sur les balises enfants
+        for child in rep.head.children:
+            print(child)
+
+    # print(res.headers)
+    # print(res.text)  #res.text si on veut afficher la pagesu
